6eea80929bb1dd42fb5a1a3334f11ab765b87d5d
Martin Alemajoh <alemajohmartin@gmail.com>
2021-11-05 12:54:54 -0400

    installed material ui library and setup it up to work with react app

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+function Header() {
+  return (
+    <div className="Header">
+      <Button variant="contained">Hello World</Button>
+    </div>
+  );
+}
+
+export default Header;
