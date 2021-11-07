399538c612f6a81f6a214ec28e30f8878e300b3a
Martin Alemajoh <alemajohmartin@gmail.com>
2021-11-07 08:06:41 -0500

    added the edit functionality to card

--- src/components/card/ExpenseCard.js
+++ src/components/card/ExpenseCard.js
@@ -5,14 +5,28 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import { Edit, Delete } from "@mui/icons-material";
 import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
 
 function ExpenseCard(props) {
+  const [editMode, setEditMode] = React.useState(false);
+
+  const handleEditMode = () => {
+    setEditMode(!editMode);
+  };
   return (
     <Card sx={{ minWidth: 275 }}>
       <CardContent>
-        <Typography variant="h1" component="h1">
-          $ 150,000
-        </Typography>
+        {!editMode ? (
+          <>
+            <Typography variant="h2" component="h2">
+              $ 150,000
+            </Typography>
+          </>
+        ) : (
+          <>
+              <TextField id="outlined-basic" label="Edit Amount" variant="outlined" sx={{ width: "100%" }}/>
+          </>
+        )}
       </CardContent>
       <CardActions>
         <Grid container spacing={2} sx={{ alignItems: "center" }}>
@@ -24,7 +38,7 @@ function ExpenseCard(props) {
           <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
             <Grid container spacing={1} sx={{ justifyContent: "flex-end" }}>
               <Grid item xs={6} sm={6} md={6} lg={6} xl={6} sx={{ maxWidth: "fit-content !important" }}>
-                <Edit sx={{ fontSize: "2.5rem", cursor: "pointer" }} />
+                <Edit sx={{ fontSize: "2.5rem", cursor: "pointer" }} onClick={handleEditMode} />
               </Grid>
               <Grid item xs={6} sm={6} md={6} lg={6} xl={6} sx={{ maxWidth: "fit-content !important" }}>
                 <Delete sx={{ fontSize: "2.5rem", cursor: "pointer" }} />
