53a09acf8911284952cdbc839daea946a7fd2e9d
Martin Alemajoh <alemajohmartin@gmail.com>
2021-11-07 09:53:45 -0500

    move stringToColor function to utility file,  populated the card with data from backend and finally gave each card a bgcolor based on its category

--- src/components/card/ExpenseCard.js
+++ src/components/card/ExpenseCard.js
@@ -6,20 +6,22 @@ import CardContent from "@mui/material/CardContent";
 import { Edit, Delete, Close } from "@mui/icons-material";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
+import {stringToColor} from "./../../util/utility";
 
 function ExpenseCard(props) {
   const [editMode, setEditMode] = React.useState(false);
-
+  const { id, amount, category } = props["expense"];
   const handleEditMode = () => {
     setEditMode(!editMode);
   };
+  const cardColor = stringToColor(category)
   return (
-    <Card sx={{ minWidth: 275 }}>
+    <Card sx={{ minWidth: 275, backgroundColor: cardColor, color: "white" }}>
       <CardContent>
         {!editMode ? (
           <>
-            <Typography variant="h2" component="h2">
-              $ 150,000
+            <Typography variant="h2" component="h2" sx={{ fontWeight: "bolder" }}>
+              $ {amount.toString().padEnd(amount.toString().length + 3, ".00")}
             </Typography>
           </>
         ) : (
@@ -31,8 +33,8 @@ function ExpenseCard(props) {
       <CardActions>
         <Grid container spacing={2} sx={{ alignItems: "center" }}>
           <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
-            <Typography variant="h4" component="h4">
-              Rent
+            <Typography variant="h4" component="h4" sx={{ textTransform: "capitalize" }}>
+              {category}
             </Typography>
           </Grid>
           <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,6 +4,7 @@ import Avatar from "@mui/material/Avatar";
 import Toolbar from "@mui/material/Toolbar";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
+import { stringToColor } from "../../util/utility";
 
 function Header() {
   const [auth] = React.useState(true);
@@ -16,26 +17,6 @@ function Header() {
   const handleClose = () => {
     setAnchorEl(null);
   };
-
-  function stringToColor(string) {
-    let hash = 0;
-    let i;
-
-    /* eslint-disable no-bitwise */
-    for (i = 0; i < string.length; i += 1) {
-      hash = string.charCodeAt(i) + ((hash << 5) - hash);
-    }
-
-    let color = "#";
-
-    for (i = 0; i < 3; i += 1) {
-      const value = (hash >> (i * 8)) & 0xff;
-      color += `00${value.toString(16)}`.substr(-2);
-    }
-    /* eslint-enable no-bitwise */
-
-    return color;
-  }
 
   function stringAvatar(name) {
     return {
