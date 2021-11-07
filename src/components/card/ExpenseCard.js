64c93b9bbdd586f11e10eab849b3902184f65d60
Martin Alemajoh <alemajohmartin@gmail.com>
2021-11-07 08:46:02 -0500

    added close icon to close form after editing in card component

--- src/components/card/ExpenseCard.js
+++ src/components/card/ExpenseCard.js
@@ -3,7 +3,7 @@ import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import { Edit, Delete } from "@mui/icons-material";
+import { Edit, Delete, Close } from "@mui/icons-material";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 
@@ -24,25 +24,35 @@ function ExpenseCard(props) {
           </>
         ) : (
           <>
-              <TextField id="outlined-basic" label="Edit Amount" variant="outlined" sx={{ width: "100%" }}/>
+            <TextField id="outlined-basic" label="Edit Amount" variant="outlined" sx={{ width: "100%" }} />
           </>
         )}
       </CardContent>
       <CardActions>
         <Grid container spacing={2} sx={{ alignItems: "center" }}>
           <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
-            <Typography variant="h3" component="h3">
+            <Typography variant="h4" component="h4">
               Rent
             </Typography>
           </Grid>
           <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
             <Grid container spacing={1} sx={{ justifyContent: "flex-end" }}>
-              <Grid item xs={6} sm={6} md={6} lg={6} xl={6} sx={{ maxWidth: "fit-content !important" }}>
-                <Edit sx={{ fontSize: "2.5rem", cursor: "pointer" }} onClick={handleEditMode} />
-              </Grid>
-              <Grid item xs={6} sm={6} md={6} lg={6} xl={6} sx={{ maxWidth: "fit-content !important" }}>
-                <Delete sx={{ fontSize: "2.5rem", cursor: "pointer" }} />
-              </Grid>
+              {!editMode ? (
+                <>
+                  <Grid item xs={6} sm={6} md={6} lg={6} xl={6} sx={{ maxWidth: "fit-content !important" }}>
+                    <Edit sx={{ fontSize: "2.5rem", cursor: "pointer" }} onClick={handleEditMode} />
+                  </Grid>
+                  <Grid item xs={6} sm={6} md={6} lg={6} xl={6} sx={{ maxWidth: "fit-content !important" }}>
+                    <Delete sx={{ fontSize: "2.5rem", cursor: "pointer" }} />
+                  </Grid>
+                </>
+              ) : (
+                <>
+                  <Grid item xs={6} sm={6} md={6} lg={6} xl={6} sx={{ maxWidth: "fit-content !important" }}>
+                    <Close onClick={handleEditMode} sx={{ fontSize: "2.5rem", cursor: "pointer" }} />
+                  </Grid>
+                </>
+              )}
             </Grid>
           </Grid>
         </Grid>
